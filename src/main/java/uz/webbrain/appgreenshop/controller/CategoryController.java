e7624d350b27858be54bb3e54db9c7a89c5f4d82
jimhome_mac <jsr1611@outlook.com>
2022-02-23 00:11:04 +0900

    commented out pre-authorise annotations

--- src/main/java/uz/webbrain/appgreenshop/controller/CategoryController.java
+++ src/main/java/uz/webbrain/appgreenshop/controller/CategoryController.java
@@ -27,39 +27,39 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
+//    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     @PostMapping
     public HttpEntity<?> addCategory(@Valid @RequestBody CategoryDTO categoryDTO){
         return categoryService.addCategory(categoryDTO);
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
+//    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     @ApiPageable
     @GetMapping
     public HttpEntity<?> getAllAsPageable(@ApiIgnore Pageable pageable){
         return categoryService.findAllPageable(pageable);
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
+//    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     @GetMapping("/list")
     public HttpEntity<?> getCategories(){
         return categoryService.findAll();
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
+//    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     @GetMapping("/{id}")
     public HttpEntity<?> getOneCategory(@PathVariable Long id){
         return categoryService.findOneById(id);
     }
 
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
+//    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     @PutMapping("/{category_id}")
     public HttpEntity<?> editCategory(@PathVariable Long category_id, @RequestBody CategoryDTO categoryDTO){
         return categoryService.editCategory(category_id, categoryDTO);
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
+//    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     @DeleteMapping("/{category_id}")
     public HttpEntity<?> deleteCategory(@PathVariable Long category_id){
         return categoryService.deleteCategory(category_id);
